bef30adcfe4a807d9ddd989b7d2a3f4c01375e1f
Amrit-Bhaskar-code <80566490+Amrit-Bhaskar-code@users.noreply.github.com>
2021-06-11 19:17:21 +0530

    Time: 8 ms (99.64%), Space: 9.9 MB (30.12%) - LeetHub

---
+++ number-of-islands/number-of-islands.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    void dfs(int i, int j, vector<vector<char>>& grid)
+    {
+        
+        if(i<0 || i==grid.size() || j<0 || j==grid[0].size() || grid[i][j]=='0')
+        {
+            return;
+        }
+        grid[i][j] = '0';
+                  
+            dfs(i+1,j, grid);
+     
+            dfs(i, j+1, grid);
+        
+            dfs(i, j-1, grid);
+       
+            dfs(i-1,j, grid);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        vector<vector<int>> visited(grid.size(), vector<int> (grid[0].size(), 0));
+        int ans = 0;
+        for(int i=0; i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    
+                    dfs(i,j, grid);
+                    ans++;
+                }
+            }
+        }
+    return ans;
+    }                                                           
+};
