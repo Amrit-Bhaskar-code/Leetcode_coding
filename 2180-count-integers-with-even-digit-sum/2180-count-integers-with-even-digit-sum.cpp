62ecd58737079f1924aa84d160a10ddac16d82a6
Amrit-Bhaskar-code <80566490+Amrit-Bhaskar-code@users.noreply.github.com>
2022-08-27 17:37:21 -0700

    Time: 4 ms (29.01%), Space: 5.9 MB (26.79%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countEven(int num) {
+       int temp = num, sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        cout<<sum;
+        return sum % 2 == 0 ? temp / 2 : (temp - 1) / 2; 
+    }
+};
